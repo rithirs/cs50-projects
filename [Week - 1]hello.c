dfc1c601f41cb67f3728120119bfd036cd427a9e
R Rithika Sharon <rithika.sharon.rajesh@gmail.com>
2025-07-04 21:09:48 +0530

    Update [Week - 1]hello.c

--- [Week - 1]hello.c
+++ [Week - 1]hello.c
@@ -1,3 +1,7 @@
+/* Implement a program in C that prompts the user for their name and then says hello to that user. 
+For instance, if the user’s name is Adele, your program should print :
+  hello, Adele\n */
+
 #include <cs50.h>
 #include <stdio.h>
 int main(void)
